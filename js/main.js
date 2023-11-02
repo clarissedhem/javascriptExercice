507136024f5be50bcd9459af331e18ff7b8a78d4
clarissedhem <clarisse.dhem@gmail.com>
2023-11-02 16:00:42 +0100

    travail du 2 novembre aprem

--- js/main.js
+++ js/main.js
@@ -1,20 +1,47 @@
-const pg = document.querySelectorAll('.replique');
+const repliques = document.getElementById('repliques');
+const pg = document.querySelectorAll('.relique');
 const r1 = document.getElementById('r1');
 const r2 = document.getElementById('r2');
 const r3 = document.getElementById('r3');
 const r4 = document.getElementById('r4');
 const selected = document.getElementById('selected');
 const haut = document.getElementById('haut');
-const bas = doxument.getElementById('bas');
+const bas = document.getElementById('bas');
+const pgTable = [
+    `Réplique 1 : <span>« Pourquoi voulez-vous boxer ?" - Parce que je ne peux pas chanter ou danser »</span> (Rocky)`,
+    `Réplique 2 : <span>« Il y a un dicton en Italie : on peut se brûler la langue avec un bol de soupe. - Il y a un dicton en Irlande : rien à foutre des dictons italiens ».</span> (Piège en eaux troubles)`,
+    `Réplique 3 : <span>« Alex, ne fait pas ça, c'est ton frère! - Pourquoi, parce qu'il me ressemble? Je vais changer ça tout de suite »</span> (Double Impact)`,
+    `Réplique 4 : <span>« T'aimes bien les omelettes ? ... Tiens, je te casse les œufs ! »</span> (Last Action Hero)`
+];
 
-// EX 2
+let clickMemory = null;
+
+function changeBackgroundColor(el) {
+    if (clickMemory === el) {
+        el.classList.toggle('yellow');
+        el.classList.toggle('white');
+        clickMemory = null;
+        console.log(clickMemory);
+    } else if (clickMemory === null) {
+        clickMemory = el;
+        el.classList.toggle('yellow');
+        el.classList.toggle('white');
+        console.log(clickMemory);
+    } else {
+        clickMemory.classList.remove('yellow');
+        clickMemory.classList.add('white');
+        clickMemory = el;
+        el.classList.toggle('yellow');
+        el.classList.toggle('white');
+        console.log(clickMemory);
+    }
+}
+
+// EX 2, 3 et 5
 pg.forEach(element => {
     element.addEventListener('click', function() {
-        if (this.classList.contains('yellow')) {
-            this.classList.remove('yellow');
-        } else {
-            this.classList.add('yellow');
-        }
+        changeBackgroundColor(this);
+        console.log(clickMemory);
     })
 });
 
@@ -60,3 +87,22 @@ r4.addEventListener('click', function() {
 })
 
 // EX 6
+let pgTableCopy = [...pgTable];
+
+haut.addEventListener('click', function() {
+    pgTableCopy.push(pgTableCopy[0]);
+    pgTableCopy.shift();
+    r1.innerHTML = pgTableCopy[0];
+    r2.innerHTML = pgTableCopy[1];
+    r3.innerHTML = pgTableCopy[2];
+    r4.innerHTML = pgTableCopy[3];
+})
+
+bas.addEventListener('click', function() {
+    pgTableCopy.unshift(pgTableCopy[3]);
+    pgTableCopy.pop();
+    r1.innerHTML = pgTableCopy[0];
+    r2.innerHTML = pgTableCopy[1];
+    r3.innerHTML = pgTableCopy[2];
+    r4.innerHTML = pgTableCopy[3];
+})
